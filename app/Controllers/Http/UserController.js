d90983d402946926d18825e19571488ae1d6ccfb
Karam Kabbara <contact@karam.io>
2019-01-19 19:48:54 +0000

    Now returning top 5 user questions for profile page.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,21 +1,35 @@
 "use strict";
 
 const User = use("App/Models/User");
+const Question = use("App/Models/Question");
 
 class UserController {
   async index({ view, request, params, session }) {
-
     // becomes "is logged in user etc"
     const user = await User.query()
       .where("username", session.get("username"))
       .firstOrFail();
-    
+
     const profileUser = await User.query()
       .where("username", params.username)
       .firstOrFail();
 
+    var questions = await Question.query()
+      .where("user_id", profileUser.id)
+      .with("category")
+      .with("upvotes", builder => {
+        builder.where("is_positive", true);
+      })
+      .with("downvotes", builder => {
+        builder.where("is_positive", false);
+      })
+      .orderBy("score", "desc")
+      .limit(5)
+      .fetch();
+
     return view.render("user/profile", {
       user: profileUser.toJSON(),
+      questions: questions.toJSON(),
       title: params.username
     });
   }
